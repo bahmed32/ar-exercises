6bace3be8aae7175507505f1ee3d8d09cb0c5308
bahmed32 <bilqis.ali32@gmail.com>
2024-01-25 00:26:17 +0000

    excersises 3-6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ApplicationRecord
+  has_many :employees
+end
+class Employee < ApplicationRecord
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,35 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :must_carry_apparel
+
+  has_many :employees
+
+  private
+
+  def must_carry_apparel
+    errors.add(:base, 'Must carry at least one of men\'s or women\'s apparel') unless mens_apparel || womens_apparel
+  end
+end
+
+class Employee < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+
+  belongs_to :store
+end
+
+validate :must_carry_apparel
+
+def must_carry_apparel
+  errors.add(:base, 'Must carry at least one of men\'s or women\'s apparel') unless mens_apparel || womens_apparel
+end
+
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
